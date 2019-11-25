99158c7850dd15b6aada74ba1087993c0a220324
Ricardo SH <davidyesc@gmail.com>
2019-11-25 06:20:42 -0600

    Corregí errores en 2 funciones
    Corregí los errores que estaba dando al generar todas las jugadas, támbien los de la función Max-Min

--- Conecta-4/conecta.c
+++ Conecta-4/conecta.c
@@ -59,11 +59,7 @@ bool gano(char tablero[6][7], char color){
     return false;
 }
 /*Funcion que devuelve el nodo al cual va a tirar*/
-int minMax(int nivel, bool buscarMax, struct nodo* nodo) {
-    if (nivel == dificultad){
-        int valor = nodo->valor;
-        return valor;
-    }
+int minMax(bool buscarMax, struct nodo* nodo) {
     int k = 0;
     bool todosNulos = true;
     for(k = 0; k < 7; k++){
@@ -73,21 +69,21 @@ int minMax(int nivel, bool buscarMax, struct nodo* nodo) {
         }
     }
     if(todosNulos){
-        return puntuarJugada(nodo->tablero, k);
+        return nodo->valor;
     }
-    if (buscarMax){
-        int i = 0,j=0;
+    if(buscarMax){
+        int i = 0, j = 0;
         int valorMax;
         for(i = 0; i < 7; i++){
             if(nodo->hijos[i] != NULL){
-                valorMax = minMax(nivel+1, false, nodo->hijos[i]);
-                nodo->siguiente=i;
+                valorMax = minMax(false, nodo->hijos[i]);
+                nodo->siguiente = i;
                 break;
             }
         }
-        for(j = i; i < 7; i++){
+        for(; i < 7; i++){
             if(nodo->hijos[i] != NULL){
-                int temp = minMax(nivel+1, false, nodo->hijos[i]);
+                int temp = minMax(false, nodo->hijos[i]);
                 if(valorMax < temp){
                     valorMax = temp;
                     nodo->siguiente =  i;
@@ -95,20 +91,20 @@ int minMax(int nivel, bool buscarMax, struct nodo* nodo) {
             }
         }
         nodo->valor = valorMax;
-        return valorMax;
+        return nodo->valor;
     }else{
-        int i = 0,j=0;
+        int i = 0;
         int valorMin;
         for(i = 0; i < 7; i++){
             if(nodo->hijos[i] != NULL){
-                valorMin = minMax(nivel+1, false, nodo->hijos[i]);
-                nodo->siguiente=i;
+                valorMin = minMax(true, nodo->hijos[i]);
+                nodo->siguiente = i;
                 break;
             }
         }
-        for(j = i; i < 7; i++){
-            if(nodo->hijos != NULL){
-                int temp = minMax(nivel+1, true, nodo->hijos[i]);
+        for(; i < 7; i++){
+            if(nodo->hijos[i] != NULL){
+                int temp = minMax(true, nodo->hijos[i]);
                 if(valorMin > temp){
                     valorMin = temp;
                     nodo->siguiente = i;
@@ -116,56 +112,56 @@ int minMax(int nivel, bool buscarMax, struct nodo* nodo) {
             }
         }
         nodo->valor = valorMin;
-        return valorMin;
+        return nodo->valor;
     }
 }
 /*Función para generar un árbol de jugadas*/
-struct nodo* generarJugadas(char tablero[6][7], int columna, int nivel, char color, struct nodo* padre){
-    struct nodo* hoja = (struct nodo*)malloc(sizeof(struct nodo));
-    if(hoja == NULL){
-        printf("Se acabo la memoria");
-        return NULL;
-    }
+void generarJugadas(char tablero[6][7], int nivel, char color, struct nodo* padre){
     if(nivel == dificultad){
         int i,j;
         for(i = 0; i < 6; i++){
-            hoja->hijos[i] = NULL;
-            for(j = 0; j < 7; j++){
-                hoja->tablero[i][j] = tablero[i][j];
-            }
+            padre->hijos[i] = NULL;
         }
-        if(tirar(hoja->tablero, color, columna)){
-            hoja->valor = puntuarJugada(hoja->tablero, columna);
-            return hoja;
+        if (color == 'A'){
+            padre->valor = puntuarJugada(padre->tablero, 'R');
         }else{
-            free(hoja);
-            return NULL;
+            padre->valor = -puntuarJugada(padre->tablero, 'A');
         }
+        return;
     }
-    int i,j;
-    for(i = 0; i < 6; i++){
-        for(j = 0; j < 7; j++){
-            hoja->tablero[i][j] = tablero[i][j];
-        }
+    int i,j,k;
+    for (i = 0; i < 7; i++){
+        padre->hijos[i] = (struct nodo*)malloc(sizeof(struct nodo));
     }
-    if(tirar(hoja->tablero, color, columna)){
-        if(gano(hoja->tablero, color)){
-            for(i = 0; i < 6; i++){
-                hoja->hijos[i] = NULL;
+    for(i = 0; i < 7; i++){
+        for(j = 0; j < 6; j++){
+            for (k = 0; k < 7; k++){
+                padre->hijos[i]->tablero[j][k] = tablero[j][k];
             }
-            return hoja;
         }
-        for(i = 0; i < 7 ; i++){
-            if(nivel%2 == 0){
-                hoja->hijos[i] = generarJugadas(hoja->tablero, i, nivel+1, 'A', hoja);
+    }
+    for(int i = 0; i < 7; i++){
+        if(tirar(padre->hijos[i]->tablero, color, i)){
+            if(!gano(padre->hijos[i]->tablero, color)){
+                if(color == 'R'){
+                    generarJugadas(padre->hijos[i]->tablero, nivel+1, 'A', padre->hijos[i]);
+                }else{
+                    generarJugadas(padre->hijos[i]->tablero, nivel+1, 'R', padre->hijos[i]);
+                }
             }else{
-                hoja->hijos[i] = generarJugadas(hoja->tablero, i, nivel+1, 'R', hoja);
+                if (color == 'A'){
+                    padre->valor = puntuarJugada(padre->tablero, 'R');
+                }else{
+                    padre->valor = -puntuarJugada(padre->tablero, 'A');
+                }
+                for (j = 0; j < 7; j++){
+                    padre->hijos[i]->hijos[j] = NULL;
+                }
             }
+        }else{
+            free(padre->hijos[i]);
+            padre->hijos[i] = NULL;
         }
-        return hoja;
-    }else{
-        free(hoja);
-        return NULL;
     }
 }
 
